eee9b379fc53eb5f9aabc6642c54c7ff4eb8c04d
Lucas <padovanil@outlook.com>
2023-04-25 21:55:21 -0300

    Adição de Arquivos Base
    Base quer dizer que é em baixo e embaixo me lembra posição de sexo

---
+++ src/main/java/io/SeaSaltSquid/SeaSaltSquidApplication.java
@@ -0,0 +1,13 @@
+package io.SeaSaltSquid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaSaltSquidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaSaltSquidApplication.class, args);
+	}
+
+}
